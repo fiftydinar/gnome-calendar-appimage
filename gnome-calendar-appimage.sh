d3618cbf126aba076c6ebe15ebdb37dddb8e7704
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-11 09:50:43 +0200

    Refactor

--- gnome-calendar-appimage.sh
+++ gnome-calendar-appimage.sh
@@ -2,93 +2,77 @@
 
 set -eux
 
+ARCH="$(uname -m)"
 PACKAGE=gnome-calendar
-DESKTOP=org.gnome.Calendar.desktop
-ICON=org.gnome.Calendar.svg
+ICON=/usr/share/icons/hicolor/scalable/apps/org.gnome.Calendar.svg
+DESKTOP=/usr/share/applications/org.gnome.Calendar.desktop
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+UPDATER="https://github.com/pkgforge-dev/AppImageUpdate-Enhanced-Edition/releases/latest/download/appimageupdatetool+validate-$ARCH.AppImage"
 
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
-export VERSION=$(pacman -Q "$PACKAGE" | awk 'NR==1 {print $2; exit}')
-echo "$VERSION" > ~/version
+VERSION=$(pacman -Q "$PACKAGE" | awk 'NR==1 {print $2; exit}')
+[ -n "$VERSION" ] && echo "$VERSION" > ~/version
 
-UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
-URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME="$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
 
 # Prepare AppDir
 mkdir -p ./AppDir/shared/lib
-cd ./AppDir
 
 # Copy desktop file & icon
-cp -v /usr/share/applications/"$DESKTOP"             ./
-cp -v /usr/share/icons/hicolor/scalable/apps/"$ICON" ./
-cp -v /usr/share/icons/hicolor/scalable/apps/"$ICON" ./.DirIcon
+cp -v "$DESKTOP"   ./AppDir/
+cp -v "$ICON"      ./AppDir/
 
 # Patch StartupWMClass to work on X11
 # Doesn't work when ran in Wayland, as it's 'org.gnome.Calendar' instead.
 # It needs to be manually changed by the user in this case.
 sed -i '/^\[Desktop Entry\]/a\
 StartupWMClass=gnome-calendar
-' "$DESKTOP"
+' ./AppDir/*.desktop
 
-# ADD LIBRARIES
-wget "$SHARUN" -O ./sharun-aio
-chmod +x ./sharun-aio
-xvfb-run -a -- ./sharun-aio l -p -v -e -s -k \
-	/usr/bin/gnome-calendar \
-        /usr/lib/evolution-data-server/*/* \
-	/usr/lib/libgweather*
-rm -f ./sharun-aio
+# DEPLOY ALL LIBS
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/gnome-calendar /usr/lib/evolution-data-server/*/* /usr/lib/libgweather*
+cp -vr /usr/share/vala ./AppDir/share/
+cp -vr /usr/share/devhelp ./AppDir/share/
 
-# Copy locale manually, as sharun doesn't do that at the moment
-cp -vr /usr/lib/locale           ./shared/lib
-cp -r /usr/share/locale          ./share
-find ./share/locale -type f ! -name '*glib*' ! -name '*gnome-calendar*' -delete
-find ./share/locale -type f 
-# Fix hardcoded path for locale
-sed -i 's|/usr/share|././/share|g' ./shared/bin/gnome-calendar
+## Fix hardcoded path for 'libcamel' libraries from 'evolution-data-server'
+sed -i 's|/usr/lib|././/lib|g' ./AppDir/shared/lib/libcamel*
+echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}' >> ./AppDir/.env
 
-# Fix hardcoded path for 'libcamel' libraries from 'evolution-data-server'
-sed -i 's|/usr/lib|././/lib|g' ./shared/lib/libcamel*
-echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}' >> ./.env
+## Copy Locations.bin & fix hardcoded path for 'libgweather' libraries
+sed -i 's|/usr/lib|././/lib|g' ./AppDir/shared/lib/libgweather*
+cp -r /usr/lib/libgweather*/ ./AppDir/shared/lib/
 
-# Copy Locations.bin & fix hardcoded path for 'libgweather' libraries
-sed -i 's|/usr/lib|././/lib|g' ./shared/lib/libgweather*
-cp -r /usr/lib/libgweather*/ ./shared/lib/
-
-# Deploy Gstreamer & evolution-data-server binaries manually, as sharun can only handle libraries in /lib/ for now
+## Deploy Gstreamer & evolution-data-server binaries manually, as sharun can only handle libraries in /lib/ for now
 echo "Deploying evolution-data-server binaries..."
-cp -r /usr/lib/evolution-data-server ./shared/lib/evolution-data-server
+cp -r /usr/lib/evolution-data-server ./AppDir/shared/lib/evolution-data-server
 
 echo "Sharunning evolution-data-server bins..."
-bins_to_find="$(find ./shared/lib/ -exec file {} \; | grep -i 'elf.*executable' | awk -F':' '{print $1}')"
+bins_to_find="$(find ./AppDir/shared/lib/ -exec file {} \; | grep -i 'elf.*executable' | awk -F':' '{print $1}')"
 for bin in $bins_to_find; do
-	mv -v "$bin" ./shared/bin && ln ./sharun "$bin"
-	echo "Sharan $bin"
+	mv -v "$bin" ./AppDir/shared/bin && ln ./sharun "$bin"
+	echo "Sharuned $bin"
 done
 
-# Prepare sharun
-ln ./sharun ./AppRun
-./sharun -g
+./quick-sharun l -g
+
+## Copy locale manually, as sharun doesn't do that at the moment
+cp -vr /usr/lib/locale           ./AppDir/shared/lib
+cp -r /usr/share/locale          ./AppDir/share
+find ./AppDir/share/locale -type f ! -name '*glib*' ! -name '*gnome-calendar*' -delete
+find ./AppDir/share/locale -type f 
+## Fix hardcoded path for locale
+sed -i 's|/usr/share|././/share|g' ./AppDir/shared/bin/gnome-calendar
+## Needed when locale patch is used
+echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}' > ./AppDir/.env
+
+## Copy the icon to AppDir's share, as it's not copied by default
+mkdir -p           ./AppDir/share/icons/hicolor/scalable/apps/
+cp -v "$ICON"      ./AppDir/"${ICON#/usr/}"
 
 # MAKE APPIMAGE WITH URUNTIME
-cd ..
-wget -q "$URUNTIME" -O ./uruntime
-chmod +x ./uruntime
-
-#Add update info to runtime
-echo "Adding update information \"$UPINFO\" to runtime..."
-./uruntime --appimage-addupdinfo "$UPINFO"
-
-echo "Generating AppImage..."
-./uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0 \
-	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S26 -B8 \
-	--header uruntime \
-	-i ./AppDir -o "$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
-
-echo "Generating zsync file..."
-zsyncmake *.AppImage -u *.AppImage
-
-echo "All Done!"
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
