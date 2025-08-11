92f6daacb3bb7944b0703031c7a95003cbd929d1
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-11 16:30:39 +0200

    Force use of `cairo` backend

--- gnome-calendar-appimage.sh
+++ gnome-calendar-appimage.sh
@@ -69,6 +69,9 @@ echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}' > ./AppDir/.env
 mkdir -p           ./AppDir/share/icons/hicolor/scalable/apps/
 cp -v "$ICON"      ./AppDir/"${ICON#/usr/}"
 
+## Force use of cairo backend
+echo "GSK_RENDERER=cairo" >> ./AppDir/.env
+
 # MAKE APPIMAGE WITH URUNTIME
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
 chmod +x ./uruntime2appimage
